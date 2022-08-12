4923c31064ef86712e427911b62fd048a6b5c054
tsekhanskaya <anutta2222@gmail.com>
2022-08-12 16:02:28 +0300

    Migrated Cart and LineItem

--- db/migrate/20220812125856_create_line_items.rb
+++ db/migrate/20220812125856_create_line_items.rb
@@ -2,7 +2,7 @@ class CreateLineItems < ActiveRecord::Migration[7.0]
   def change
     create_table :line_items do |t|
       t.integer :product_id
-      t.integer :quantity
+      t.integer :quantity, default: 1
       t.integer :cart_id
       t.integer :order_id
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_12_112502) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_12_125856) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,20 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_12_112502) do
 
   create_table "cart_statuses", force: :cascade do |t|
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "quantity", default: 1
+    t.integer "cart_id"
+    t.integer "order_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
