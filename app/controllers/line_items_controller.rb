352b2150ab11a168e54908a4fad3a8b013a239dc
tsekhanskaya <anutta2222@gmail.com>
2022-08-12 16:18:20 +0300

    Added LineItems controller

---
+++ app/controllers/line_items_controller.rb
@@ -0,0 +1,9 @@
+class LineItemsController < ApplicationController
+  def create; end
+
+  def add_quantity; end
+
+  def reduce_quantity; end
+
+  def destroy; end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,100 +10,99 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_12_125856) do
+ActiveRecord::Schema[7.0].define(version: 20_220_812_125_856) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "brands", force: :cascade do |t|
-    t.string "title"
-    t.string "bytitle"
-    t.string "description"
-    t.string "img", default: "no_image.jpg"
-    t.string "link"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'brands', force: :cascade do |t|
+    t.string 'title'
+    t.string 'bytitle'
+    t.string 'description'
+    t.string 'img', default: 'no_image.jpg'
+    t.string 'link'
+    t.string 'address'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "cart_statuses", force: :cascade do |t|
-    t.string "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'cart_statuses', force: :cascade do |t|
+    t.string 'status'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "carts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'carts', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "line_items", force: :cascade do |t|
-    t.integer "product_id"
-    t.integer "quantity", default: 1
-    t.integer "cart_id"
-    t.integer "order_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'line_items', force: :cascade do |t|
+    t.integer 'product_id'
+    t.integer 'quantity', default: 1
+    t.integer 'cart_id'
+    t.integer 'order_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.integer "restaurant_id"
-    t.integer "product_id"
-    t.integer "count"
-    t.boolean "soft_delete"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "status_id"
+  create_table 'orders', force: :cascade do |t|
+    t.integer 'restaurant_id'
+    t.integer 'product_id'
+    t.integer 'count'
+    t.boolean 'soft_delete'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'status_id'
   end
 
-  create_table "products", force: :cascade do |t|
-    t.integer "category_id"
-    t.integer "brand_id"
-    t.string "title"
-    t.string "bytitle"
-    t.text "content"
-    t.float "price"
-    t.float "old_price"
-    t.integer "status"
-    t.string "keywords"
-    t.string "description"
-    t.string "img", default: "no_image.jpg"
-    t.integer "hit", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'products', force: :cascade do |t|
+    t.integer 'category_id'
+    t.integer 'brand_id'
+    t.string 'title'
+    t.string 'bytitle'
+    t.text 'content'
+    t.float 'price'
+    t.float 'old_price'
+    t.integer 'status'
+    t.string 'keywords'
+    t.string 'description'
+    t.string 'img', default: 'no_image.jpg'
+    t.integer 'hit', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "restaurants", force: :cascade do |t|
-    t.string "title"
-    t.string "bytitle"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'restaurants', force: :cascade do |t|
+    t.string 'title'
+    t.string 'bytitle'
+    t.string 'address'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.integer 'sign_in_count', default: 0, null: false
+    t.datetime 'current_sign_in_at'
+    t.datetime 'last_sign_in_at'
+    t.string 'current_sign_in_ip'
+    t.string 'last_sign_in_ip'
+    t.string 'confirmation_token'
+    t.datetime 'confirmed_at'
+    t.datetime 'confirmation_sent_at'
+    t.string 'unconfirmed_email'
+    t.integer 'failed_attempts', default: 0, null: false
+    t.string 'unlock_token'
+    t.datetime 'locked_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+    t.index ['unlock_token'], name: 'index_users_on_unlock_token', unique: true
   end
-
 end
