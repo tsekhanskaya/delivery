d874b71f8528964f2faf2183907ae432eb5019fb
tsekhanskaya <anutta2222@gmail.com>
2022-08-12 17:20:23 +0300

    Added logic to Cart class

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,4 +4,13 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
   has_many :products, through: :line_items
+
+  # LOGIC
+  def sub_total
+    sum = 0
+    line_items.each do |line_item|
+      sum += line_item.total_price
+    end
+    sum
+  end
 end
