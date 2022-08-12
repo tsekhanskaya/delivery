142e9db546114f0bb3674e9f374558f9f87d597a
tsekhanskaya <anutta2222@gmail.com>
2022-08-12 16:56:37 +0300

    Refactoring application controller for def current_cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
 
+  before_action :authenticate_user!
+  before_action :current_cart
   # def after_sign_in_path_for(resource)
   #   root_path
   # end
@@ -13,5 +17,23 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(_resource_or_scope)
     root_path
+  end
+
+  private
+
+  def current_cart
+    if session[:cart_id]
+      cart = Cart.find_by(id: session[:cart_id])
+      if cart.present?
+        @current_cart = cart
+      else
+        session[:cart_id] = nil
+      end
+    end
+
+    if session[:cart_id].nil?
+      @current_cart = Cart.create
+      session[:cart_id] = @current_cart.id
+    end
   end
 end
